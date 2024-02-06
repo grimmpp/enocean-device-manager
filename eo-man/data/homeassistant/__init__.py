be9bff4b4850809a1074e8fde2f4b1e7e4e580f0
Philipp Grimm <grimm.philipp@gmail.com>
2024-02-06 22:28:51 +0100

    package paths fixed and homeassistant mock created

---
+++ eo-man/data/homeassistant/__init__.py
@@ -0,0 +1,2 @@
+# ist is a mock homeassistant package to not need to install the whole homeassistant for the const file. 
+# this could easily be removed and replaced by installing the original package.
