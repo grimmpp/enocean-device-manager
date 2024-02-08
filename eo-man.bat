a7ffa5e54adf5dce4b27cff100032fb42137c84c
Philipp Grimm <grimm.philipp@gmail.com>
2024-02-08 21:12:00 +0100

    created start file for windows

--- eo-man.bat
+++ eo-man.bat
@@ -1,4 +1,4 @@
-REM Starts eo-man after installed globally on windows.
+REM Starts eo-man after installed globally on windows. (pip.exe install eo-man)
 REM This file can be used to e.g. create a shortcut on the taskbar
 @echo off
 START "eo-man" "pythonw.exe" -m eo-man
